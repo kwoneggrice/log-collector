db49f56517231735e22957b5f016999fb7054268
jeongin <kwjddls1919@gmail.com>
2023-10-22 18:44:05 +0900

    데이터 모델 추가

--- LogCollectorLib/Models/MachineData.cs
+++ LogCollectorLib/Models/MachineData.cs
@@ -1,7 +1,13 @@
-﻿namespace LogCollectorLib.Models
+﻿using System.Text.Json;
+
+namespace LogCollectorLib.Models
 {
 	public class MachineData
 	{
+		public string MachineNumber { get; set; }
+		public bool isRunning { get; set; }
 
+		public string ToJsonString() => JsonSerializer.Serialize(this);
+		public static MachineData? Parse(string json) => JsonSerializer.Deserialize<MachineData>(json);
 	}
 }
